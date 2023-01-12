183897c626f3d232bd2e700b2dad6d624e60d64b
Alperen Tan <56079264+alperentan@users.noreply.github.com>
2023-01-12 16:17:23 +0300

    Update settings.py

--- Yazlab1-Website for PDF Mining/pdfyazlab/pdfyazlabpro/settings.py
+++ Yazlab1-Website for PDF Mining/pdfyazlab/pdfyazlabpro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hj3%+s@sh!wst(+(nb71jfd42kv394x78tcnpi&cgox7sw_af7'
+SECRET_KEY = 'KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
